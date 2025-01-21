38ca0dbb7a0d30193874235ca96023b0f869cf51
James Cuadra <jjlcuadradev@gmail.com>
2025-01-21 10:20:28 +0800

    Added intersection observer animation on about me section

---
+++ src/animation.js
@@ -0,0 +1,44 @@
+const fadeUpAnimate = document.querySelectorAll("#fade-up-animation");
+const titleSlide = document.querySelectorAll("#title-slide");
+
+// Fade Up Animation
+const fadeObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const intersecting = entry.isIntersecting;
+      entry.target.classList.toggle("fade-up-animate", intersecting);
+
+      if (entry.isIntersecting) {
+        fadeObserver.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+fadeUpAnimate.forEach((element) => {
+  fadeObserver.observe(element);
+});
+
+// Title Slide Animation
+const slideObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      const intersecting = entry.isIntersecting;
+      entry.target.classList.toggle("title-slide", intersecting);
+
+      if (entry.isIntersecting) {
+        slideObserver.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.8,
+  }
+);
+
+titleSlide.forEach((element) => {
+  slideObserver.observe(element);
+});
