2cde68ff59ea21232f86a091435103282a0b7eca
James Cuadra <jjlcuadradev@gmail.com>
2025-01-22 11:29:18 +0800

    Added contact section on footer

---
+++ src/icons.js
@@ -0,0 +1,9 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import {
+  faGithub,
+  faFacebookMessenger,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithub, faFacebookMessenger, faLinkedinIn);
+dom.watch();
